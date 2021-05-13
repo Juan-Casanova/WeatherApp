ef43d99e0f99b05ff5a9b54b7a51acfc2d9467d2
Juan-Casanova <casanovajm199621@gmail.com>
2021-05-13 01:39:02 -0500

    cambio de CORS3

--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -2,7 +2,7 @@ const getWeather = (lat,long) => {
 
     const url = `http://api.weatherapi.com/v1/current.json?key=5d948657eb02450cb4851307211105&q=${lat},${long}`
 
-    return fetch(url,{method:'GET'}).then(res => res.json())
+    return fetch(url,{method:'GET'},{headers: {'Content-Type':'application/json'}}).then(res => res.json())
 
 }
 
