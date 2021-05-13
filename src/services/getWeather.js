4119049ac65425a74892cf13b72f73c4e9464142
Juan-Casanova <casanovajm199621@gmail.com>
2021-05-13 01:50:18 -0500

    cambio de CORS8

--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -2,7 +2,7 @@ const getWeather = (lat,long) => {
 
     const url = `https://api.weatherapi.com/v1/current.json?key=5d948657eb02450cb4851307211105&q=${lat},${long}`
 
-    return fetch(url,{method:'POST',headers: {'Content-Type':'application/json'}, mode: 'no-cors'}).then(res => res.json())
+    return fetch(url,{method:'POST',headers: {'Content-Type':'application/json'}}).then(res => res.json())
 
 }
 
